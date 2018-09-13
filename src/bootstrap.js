1f97425d1d920201a24f661bfdc47290d309ab88
terenitup <terentaniuchi@gmail.com>
2018-09-13 10:37:51 -0600

    set up ecom project

---
+++ src/bootstrap.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './style/main.scss';
+
+import Layout from './components/layout';
+
+
+function main() {
+  ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <BrowserRouter>
+        <Layout>
+        <p>hey this is a child component</p>
+        <h2>HEY there</h2>
+        </Layout>
+      </BrowserRouter>
+    </Provider>
+    , document.querySelector('.app-wrapper'));
+}
+
+document.addEventListener('DOMContentLoaded', main);
